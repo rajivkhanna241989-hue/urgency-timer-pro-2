0c60297e64cac158dc3bcafb3df08a82c6584707
Rajiv Khanna <rajivkhanna@Rajivs-MacBook-Air-2.local>
2026-03-11 19:36:14 +1100

    Fix webhook HMAC verification

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -1,7 +1,17 @@
-import { authenticate } from "../shopify.server";
+import crypto from "crypto";
 
 export const action = async ({ request }) => {
-  const { topic, shop } = await authenticate.webhook(request);
-  console.log(`Received compliance webhook: ${topic} for ${shop}`);
+  const hmac = request.headers.get("x-shopify-hmac-sha256");
+  const body = await request.text();
+  
+  const hash = crypto
+    .createHmac("sha256", process.env.SHOPIFY_API_SECRET)
+    .update(body, "utf8")
+    .digest("base64");
+
+  if (hash !== hmac) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   return new Response(null, { status: 200 });
 };
