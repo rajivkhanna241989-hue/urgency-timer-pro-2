670ebf9cd66e7cf8fb039b6caf8fbb9ac485281a
Rajiv Khanna <rajivkhanna@Rajivs-MacBook-Air-2.local>
2026-03-11 17:16:31 +1100

    Add GDPR compliance webhooks

---
+++ app/routes/webhooks.customers.data_request.jsx
@@ -0,0 +1,7 @@
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }) => {
+  const { shop, session, topic } = await authenticate.webhook(request);
+  console.log(`Received ${topic} webhook for ${shop}`);
+  return new Response();
+};
