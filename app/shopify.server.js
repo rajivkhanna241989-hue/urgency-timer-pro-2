1663f218c1bfd9a110f456e297c9e2d08ecb88f6
Rajiv Khanna <rajivkhanna@Rajivs-MacBook-Air-2.local>
2026-03-11 15:04:40 +1100

    Fix duplicate imports

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -6,11 +6,6 @@ import {
 } from "@shopify/shopify-app-react-router/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
-import {
-  ApiVersion,
-  AppDistribution,
-  shopifyApp,
-} from "@shopify/shopify-app-react-router/server";
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
@@ -21,8 +16,6 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
- 
-
   future: {
     expiringOfflineAccessTokens: true,
   },
